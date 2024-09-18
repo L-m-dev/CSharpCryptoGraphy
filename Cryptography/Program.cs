879535158f42e7d368638b3009600f9273ef0e91
Nice <lucas.muller.acad@gmail.com>
2024-09-18 00:17:48 -0300

    added MD5 and SHA hashing

--- Cryptography/Program.cs
+++ Cryptography/Program.cs
@@ -11,8 +11,10 @@ using System.Security.Cryptography;
 using System.Text;
 
 var message = "Alef went to high school";
+
+//Checking contents.
 byte[] msgAsBytes = Encoding.UTF8.GetBytes(message);
- Console.WriteLine(msgAsBytes);
+Console.WriteLine(msgAsBytes);
 
 var initializationVector = RandomNumberGenerator.GetBytes(16);
 var key = RandomNumberGenerator.GetBytes(32);
@@ -21,17 +23,15 @@ var encryptedData = AesCbcEncryption.Encrypt(message.ToBytes(), key, initializat
 var decryptedData = AesCbcEncryption.Decrypt(encryptedData, key, initializationVector);
 
 Console.WriteLine(decryptedData.BytesToString());
-Console.ReadLine();
-//
-
+ 
 //testing RSA 
 
 var rsa = new RsaEncryption();
 
-//createPRivateKey
+//create PrivateKey. The second value, password, should be secret
 byte[] encryptedPrivateKey = rsa.ExportPrivateKey(100, "alembik");
 
-//export public Key
+//export Public Key
 byte[] publicKey = rsa.ExportPublicKey();
 
 message = "Encryption is okay";
@@ -39,20 +39,48 @@ message = "Encryption is okay";
 var encryptedMessage = rsa.Encrypt(message);
 
 //Creating new encryptor that will decrypt this...
-var rsa2 = new RsaEncryption();
+var rsa2Receiver = new RsaEncryption();
 //import the other guy's public key.
-rsa2.ImportPublicKey(publicKey);
+rsa2Receiver.ImportPublicKey(publicKey);
 
-rsa2.ImportPrivateKey(encryptedPrivateKey, "alembik");
+//Import Private Key. Unsure about this line.
+rsa2Receiver.ImportPrivateKey(encryptedPrivateKey, "alembik");
 
-var decryptedMessage = rsa2.Decrypt(encryptedMessage).BytesToString();
-
-
+var decryptedMessage = rsa2Receiver.Decrypt(encryptedMessage).BytesToString();
 Console.WriteLine(decryptedMessage);
+
+//MD5 Hashes
+
+
+MD5 md5 = MD5.Create();
+
+message = "Joy in hashing";
+
+var data = md5.ComputeHash(Encoding.UTF8.GetBytes(message));
+
+var resultHash = GenerateHashString(data); 
+Console.WriteLine(resultHash);
+ 
+//sha256 Hashes
+
+SHA256 sHA256 = SHA256.Create();
+data = sHA256.ComputeHash(Encoding.UTF8.GetBytes(message));
+resultHash = GenerateHashString(data);
+Console.WriteLine(resultHash);
+
+
 Console.ReadLine();
 
-
-
+//helper function
+string GenerateHashString(byte[] data)
+{
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < data.Length; i++)
+    {
+        sb.Append(data[i].ToString("x2")); // 
+    }
+    return sb.ToString();
+}
 
 
 
